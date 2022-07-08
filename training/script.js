f5ff4a30fbcaee278eed5e9392eac64fada32b9a
Daniil <astakhov.daniil.spb@gmail.com>
2022-07-08 17:55:32 +0300

    New ptoject 1

--- training/script.js
+++ training/script.js
@@ -1,2 +1,26 @@
-let numberOfFilms = 0;
-alert("Сколько фильмов вы смотрели?: ")
+let numberOfFilms = +prompt('Сколько фильмов ты посмотрел?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [
+
+    ],
+    privat: false
+
+};
+
+let questionMovie = prompt('Назови один из последних просмотренных фильмов', ''),
+    questionRating = prompt('Насколько оценишь на .. из 10?', ''),
+    questionMovie2 = prompt('Назови один из последних просмотренных фильмов', ''),
+    questionRating2 = prompt('Насколько оценишь на .. из 10?', '');
+
+personalMovieDB.movies[questionMovie] = questionRating;
+personalMovieDB.movies[questionMovie2] = questionRating2;
+
+console.log(personalMovieDB);
